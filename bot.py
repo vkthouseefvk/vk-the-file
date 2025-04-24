ad5d1b14b2e58df9a4f5668ed65155921e47754a
vkthouseefvk <131037814+vkthouseefvk@users.noreply.github.com>
2025-04-24 21:59:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media, Media2, choose_mediaDB, db as clientDB
-from database.users_chats_db import db , utils as pyroutils
+from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, SECONDDB_URI
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
